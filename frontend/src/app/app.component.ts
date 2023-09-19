3940bf5e5c77c417bba357846ccb444b805f5a1e
Tomasz3976 <tomeczek2886@gmail.com>
2023-09-19 18:38:29 +0200

    feat(frontend) add service injection into AppComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServerService } from './service/server.service';
+import { CustomResponse } from './interface/custom-response';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { AppState } from './interface/app-state';
+import { DataState } from './enum/data-state.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'serverapp';
+export class AppComponent implements OnInit {
+
+  appState$: Observable<AppState<CustomResponse>>;
+
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void {
+      this.appState$ = this.serverService.servers$
+      .pipe(
+        map(response => {
+          return { dataState: DataState.LOADED_STATE, appData: response }
+        }),
+        startWith({ dataState: DataState.LOADING_STATE }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error })
+        })
+      )
+  }
 }
